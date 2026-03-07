47097f1d120690f8381aae21e3079611f92ff180
Rodrigaless <rjdiazmartino@gmail.com>
2026-03-07 22:16:40 +0100

    Add Climatizacion class for emission source

---
+++ Climatizacion.java
@@ -0,0 +1,12 @@
+/**
+ * Fuente de emisión por uso de sistemas de climatización.
+ */
+public class Climatizacion extends FuenteEmision {
+
+    private double horasUso;
+
+    /** @param horasUso horas de funcionamiento del sistema */
+    public Climatizacion(String descripcion, double factorEmision, double costeUnitario, double horasUso) {
+        super(descripcion, factorEmision, costeUnitario);
+        this.horasUso = horasUso;
+    }
