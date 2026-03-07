dcf06449be05e37561be6905e917db122a75215b
Rodrigaless <rjdiazmartino@gmail.com>
2026-03-07 22:04:09 +0100

    Create Main class with initial project structure
    Initial version of the Main class with basic structure.

---
+++ Main.java
@@ -0,0 +1,17 @@
+/**
+ * Clase principal del proyecto de Programación.
+ * Esta versión inicial contiene la estructura básica para el desarrollo posterior.
+ * * @author Rodrigo
+ * @version 1.0
+ */
+public class Main {
+
+    /**
+     * Punto de entrada principal de la aplicación.
+     * @param args Argumentos de la línea de comandos.
+     */
+    public static void main(String[] args) {
+        // TODO: Implementar la lógica principal en la versión intermedia
+        System.out.println("Proyecto de Programación - Versión Inicial");
+    }
+}
